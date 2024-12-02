7245216f3562ff00de17c2aa630b8cfa84be2407
pstridsb1 <pierre.stridsberg@astrazeneca.com>
2024-12-02 12:39:19 +0100

    Removes comment

--- day02/python/src/test/aoc_test.py
+++ day02/python/src/test/aoc_test.py
@@ -8,8 +8,6 @@ class MyTestCase(unittest.TestCase):
     def test_part1(self):
         aoc.start('../../../input.txt', 'part2')
 
-        #part2 758 to high | 398 to low, right for someone else.
-
 
 
 if __name__ == '__main__':
